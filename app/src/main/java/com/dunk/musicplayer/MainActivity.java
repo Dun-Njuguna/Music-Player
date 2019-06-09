bb985ea108b06fdae298b26f8a4b12086914cec5
Dun-Njuguna <duncankariuki60@gmail.com>
2019-06-09 20:19:54 +0300

    include functionality to display all user songs

--- app/src/main/java/com/dunk/musicplayer/MainActivity.java
+++ app/src/main/java/com/dunk/musicplayer/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dunk.musicplayer;
 
 import android.Manifest;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -14,15 +17,23 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.File;
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String[] itemsAll;
+    @BindView(R.id.list_item)ListView songsList;
+
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         appExternalStoragePermission();
-
     }
 
     public void appExternalStoragePermission()
@@ -32,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override public void onPermissionGranted(PermissionGrantedResponse response)
                     {
-
+                        displayAudioSongsName();
                     }
                     @Override public void onPermissionDenied(PermissionDeniedResponse response)
                     {
@@ -40,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token)
                     {
-
+                        token.cancelPermissionRequest();
                     }
                 }).check();
     }
@@ -48,7 +59,48 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<File> readAudioFiles(File file)
     {
-        ArrayList<File> arrayList = new
+        ArrayList<File> arrayList = new ArrayList<>();
+
+        File[] allFiles = file.listFiles();
+        for (File individualFile: allFiles)
+        {
+            if (individualFile.isDirectory() && !individualFile.isHidden())
+            {
+               arrayList.addAll(readAudioFiles(individualFile));
+            }
+            else
+            {
+                if (individualFile.getName().endsWith(".mp3") ||
+                        individualFile.getName().endsWith(".aac") ||
+                        individualFile.getName().endsWith(".wav") ||
+                        individualFile.getName().endsWith(".wma")
+                )
+                {
+                    arrayList.add(individualFile);
+                }
+            }
+        }
+
+        return arrayList;
+    }
+
+
+    private void  displayAudioSongsName()
+    {
+        final ArrayList<File> audioSongs = readAudioFiles(Environment.getExternalStorageDirectory());
+
+        itemsAll = new String[audioSongs.size()];
+        for (int songCounter = 0; songCounter<audioSongs.size(); songCounter++)
+        {
+            itemsAll[songCounter] = audioSongs.get(songCounter).getName();
+        }
+
+        arrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_expandable_list_item_1, itemsAll);
+
+        if (arrayAdapter !=null)
+            songsList.setAdapter(arrayAdapter);
+
     }
 
 
--- app/src/main/java/com/dunk/musicplayer/SmartPlayerActivity.java
+++ app/src/main/java/com/dunk/musicplayer/SmartPlayerActivity.java
@@ -126,6 +126,4 @@ public class SmartPlayerActivity extends AppCompatActivity {
         }
     }
 
-
-
 }
